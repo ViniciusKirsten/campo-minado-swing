2c27f67f7a9cd5152f687670bf271a353c355f23
viniciuskirsten <vinicius.k.mendonca@outlook.com>
2023-01-21 19:34:08 -0300

    alguns testes realizados

--- src/br/com/viniciusmendonca/cm/visao/TelaPrincipal.java
+++ src/br/com/viniciusmendonca/cm/visao/TelaPrincipal.java
@@ -8,10 +8,10 @@ import br.com.viniciusmendonca.cm.modelo.Tabuleiro;
 public class TelaPrincipal extends JFrame{
 
 	public TelaPrincipal() {
-		Tabuleiro tabuleiro = new Tabuleiro(16,13,100);//(16,13)480 quadrados
+		Tabuleiro tabuleiro = new Tabuleiro(52,52,5);//(16,13)480 quadrados
 		add(new PainelTabuleiro(tabuleiro));
 		
-		setTitle("Campo Minado");
+		setTitle("Sara");
 		setSize(690, 438);
 		setLocationRelativeTo(null); // centralizar a tela sem nenhuma referencia
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE); //fechar assim que cliclar no "x"
